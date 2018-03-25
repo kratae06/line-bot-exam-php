dd3779d2fcb9c61e8ffc8baf522dca6596ee5771
kratae06 <37654150+kratae06@users.noreply.github.com>
2018-03-25 14:42:50 +0700

    Update databata.php

--- databata.php
+++ databata.php
@@ -4,11 +4,11 @@ $username = "spaShoes";
 $password = "password";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 echo "Connected successfully";
 ?>
