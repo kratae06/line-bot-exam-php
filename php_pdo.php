52bcf3c36e5ab6c45f03a748279d6eca5c4591e9
kratae06 <37654150+kratae06@users.noreply.github.com>
2018-03-26 14:10:49 +0700

    Add files via upload

---
+++ php_pdo.php
@@ -0,0 +1,24 @@
+<?php
+
+	$HOST_NAME = "localhost";
+	$DB_NAME = "php_pdo";
+	$CHAR_SET = "charset=utf8"; // เช็ตให้อ่านภาษาไทยได้
+
+	$USERNAME = "root";     // ตั้งค่าตามการใช้งานจริง
+	$PASSWORD = "smit001";  // ตั้งค่าตามการใช้งานจริง
+
+
+	try {
+	
+		$db = new PDO('mysql:host='.$HOST_NAME.';dbname='.$DB_NAME.';'.$CHAR_SET,$USERNAME,$PASSWORD);
+	
+		echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
+	
+	
+	} catch (PDOException $e) {
+	
+		echo "ไม่สามารถเชื่อมต่อฐานข้อมูลได้ : ".$e->getMessage();
+	
+	}
+ 
+?>
