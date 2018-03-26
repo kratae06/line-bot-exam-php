c18e8ada1b069e25cda5c782f2f10f560556b971
kratae06 <37654150+kratae06@users.noreply.github.com>
2018-03-26 15:11:36 +0700

    Update php1.php

--- php1.php
+++ php1.php
@@ -2,6 +2,6 @@
 	$hostName="127.0.0.1";
 	$user="root";
 	$dbName="php_pdo";
-	$connect= mysqli_connect($hostName,$user,$pass,$dbName) or die("ไม่สามารถเชิ่อมต่อฐานข้อมูลได้");
+	$connect= mysqli_connect($hostName,$user,$dbName) or die("ไม่สามารถเชิ่อมต่อฐานข้อมูลได้");
 	echo "ติดต่อฐานข้อมูลเรียบร้อย";
 ?>
